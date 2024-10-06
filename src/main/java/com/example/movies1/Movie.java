62d1e4255555417dc74fba0d4624b40958fd1bc7
Avinda Shamal <shamalgba.21@uom.lk>
2024-10-06 15:30:01 +0530

    Merge branch 'main' of https://github.com/AvindaShamal/MoviesAPI
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/example/movies1/Movie.java
@@ -0,0 +1,28 @@
+package com.example.movies1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "Movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private Object Id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Reviews> reviewIds;
+}
