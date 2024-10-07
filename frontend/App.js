78342fd8faadbe0c898de4fa8db0ea7587b3aaee
Avinda Shamal <shamalgba.21@uom.lk>
2024-10-07 11:19:13 +0530

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import Header from './components/header/Header';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get('http://localhost:8080/api/v1/movies');
+      
+      setMovies(response.data);
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+}
+
+useEffect(() => {
+  getMovies();
+}, []);
+
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path='/' element={<Home movies={movies}/>}> </Route>
+          
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
